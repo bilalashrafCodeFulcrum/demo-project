391f8284329d866402f012f8307491195b4f0441
bilal ashraf <bilal.ashraf@codefulcrum.com>
2022-05-19 21:52:43 +0500

    packages setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {TextField} from '@mui/material'
 
 function App() {
   return (
     <div className="">
-      <h1> this is demo project </h1>
+      <h1 className='text-center mt-10 text-green-400'> this is demo project </h1>
+      <TextField
+      name='field'
+      label='First Name'
+      fullWidth
+      />
     </div>
   );
 }
